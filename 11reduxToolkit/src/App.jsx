1c60f8ec669333176d7699a564acf3c6c19064e2
Mansoor Ahmed <colabwithmansoor@gmail.com>
2026-06-28 22:56:16 +0500

    Add TodoReduxToolkit Project

---
+++ 11reduxToolkit/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import AddTodo from './Components/AddTodo'
+import Todo from './Components/Todo'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Learn Redux Toolkit</h1>
+     <AddTodo/>
+     <Todo/>
+    </>
+  )
+}
+
+export default App
